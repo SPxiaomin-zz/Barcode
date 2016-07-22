9d73e1a4dc4d07c5c3fbbc2d6758ce84240fb5b1
yang yao <yang@livepay.my>
2016-07-22 13:39:41 +0800

    It's working.

--- android/app/src/main/java/com/barcode/MainApplication.java
+++ android/app/src/main/java/com/barcode/MainApplication.java
@@ -1,14 +1,11 @@
 package com.barcode;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-import com.eguma.barcodescanner.BarcodeScannerPackage;
+
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,8 +21,6 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new BarcodeScannerPackage()
       );
     }
   };
